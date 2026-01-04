a2d12739059491a9d00f0ea4b0fe0eb9ae5f31ba
Joelle Linder <joelsc.linder@gmail.com>
2026-01-04 22:47:21 +0100

    Added common gnustep env variables to source

---
+++ Blackb0x/gnustep-env.sh
@@ -0,0 +1,6 @@
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+export PATH=/usr/local/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export LD=/usr/bin/ld.lld
+export LDFLAGS=-fuse-ld=/usr/bin/ld.lld
